d6cf1c2fa47dc540c4054c0d2ed7ca5ad667b099
yuansudong <1145919989@qq.com>
2023-04-21 23:45:03 +0800

    初次提交

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps : [{
+      script: 'index.js',
+    }], 
+    // Deployment Configuration
+    deploy : {
+      production : {
+         "user" : "ubuntu",
+         "host" : ["1.14.1.37"],
+         "ref"  : "origin/master",
+         "repo" : "git@github.com:yuansudong/pm2.git",
+         "path" : "/home/yuansudong",
+         "post-deploy" : "npm install"
+      }
+    }
+  };
