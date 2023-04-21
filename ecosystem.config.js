4f0ff9113d0d93a680f1029037736110802bb74b
yuansudong <1145919989@qq.com>
2023-04-22 00:32:23 +0800

    hello

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,7 +14,7 @@ module.exports = {
          "post-setup": "echo '安装之后,会在远程服务器上执行这条命令' > ~/post-setup.text",
          "pre-deploy" : "echo '在更新之前会在远程服务器上执行这条命令 12'",  // 更新HOOK, 在拉取代码之前,会在远程服务器上执行这条命令 
          "post-deploy" : "npm install", // 更新HOOK, 在拉取代码之后,会在远程的服务器上执行这条命令
-         "pre-deploy-local" : "echo '在更新之前' > ~/a.txt" // 更新HOOK,在更新之前,会在本地的服务上执行这条命令.
+         "pre-deploy-local" : "echo '在更新之前或者部署之前' > ~/a.txt" // 更新HOOK,在更新之前,会在本地的服务上执行这条命令.
       }
     }
   };
