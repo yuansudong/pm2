4dba9f03d7c36e0a14ee1a3250fd048f469b8320
yuansudong <1145919989@qq.com>
2023-04-22 00:20:31 +0800

    hello

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,13 +6,13 @@ module.exports = {
     deploy : {
       production : {
          "user" : "ubuntu",
-         "host" : ["1.14.1.37"],
-         "ref"  : "origin/master",
+         "host" : ["1.14.1.37"], // 指定服务器地址
+         "ref"  : "origin/master", // 指定分支
          "repo" : "git@github.com:yuansudong/pm2.git",
          "path" : "/home/ubuntu/new",
-         "pre-deploy" : "echo '在更新之前会在远程服务器上执行这条命令 12' > ~/a.txt",
-         "post-deploy" : "npm install",
-         "pre-deploy-local" : "echo '我怎么没有看见你呢' > ~/a.txt"
+         "pre-deploy" : "echo '在更新之前会在远程服务器上执行这条命令 12'", // 
+         "post-deploy" : "npm install", // 在更新之后,会在远程的服务器上执行这条命令
+         "pre-deploy-local" : "echo '在更新之前' > ~/a.txt" // 在更新之前,会在本地的服务上执行这条命令
       }
     }
   };
